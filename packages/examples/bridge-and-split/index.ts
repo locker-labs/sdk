cc3ecff8dce3c89d3628f29049d6bc877110adec
Mani Brar <manibrar@Manis-MacBook-Pro.local>
2025-03-28 03:22:12 +0530

    chore(examples): refactor for splits client

--- packages/examples/bridge-and-split/index.ts
+++ packages/examples/bridge-and-split/index.ts
@@ -1,45 +1,92 @@
 import { Keypair } from '@solana/web3.js';
 import bs58 from 'bs58';
-import { bridgeTokenFromSolana, CIRCLE_CONFIG, type IBridgeFromSolanaResponse, type IBridgeName, type ISolanaNetwork, type ICctpBridgeFromSolanaResponse } from "@locker-labs/sdk";
+import { type Address } from "viem";
+import { baseSepolia } from "viem/chains";
+import { LocalAccountSigner } from "@aa-sdk/core";
+import {
+  bridgeTokenFromSolana,
+  CIRCLE_CONFIG,
+  createLockerSplitClient,
+  type IBridgeFromSolanaResponse,
+  type IBridgeName,
+  type ISolanaNetwork,
+  type ICctpBridgeFromSolanaResponse,
+} from "@locker-labs/sdk";
 
 /*
-* Load environment variables
-*/
-const solanaPrivateKeyB58 = process.env.SOLANA_PRIVATE_KEY_BS58
+ * Load environment variables
+ */
+const solanaPrivateKeyB58 = process.env.SOLANA_PRIVATE_KEY_BS58;
 if (!solanaPrivateKeyB58) {
-    throw new Error("SOLANA_PRIVATE_KEY_BS58 is not set")
+  throw new Error("SOLANA_PRIVATE_KEY_BS58 is not set");
 }
 
-const solanaRpcUrl = process.env.SOLANA_RPC_URL
+const solanaRpcUrl = process.env.SOLANA_RPC_URL;
 if (!solanaRpcUrl) {
-    throw new Error("SOLANA_RPC_URL is not set")
+  throw new Error("SOLANA_RPC_URL is not set");
 }
 
 /*
-* Runtime configs
-*/
-const solanaNetwork: ISolanaNetwork = "devnet"
-const usdcMintAddress = CIRCLE_CONFIG[solanaNetwork].usdcAddress
-const usdcAmount = 1
-const recipientChain = "base"
-const recipientAddress = "0xF445b07Aad98De9cc2794593B68ecD4aa5f81076"
-const bridgeName: IBridgeName = 'cctp'
+ * Runtime configs
+ */
+const solanaNetwork: ISolanaNetwork = "devnet";
+const usdcMintAddress = CIRCLE_CONFIG[solanaNetwork].usdcAddress;
+const usdcAmount = 1;
+const recipientChain = "base";
+const recipientAddress = "0xF445b07Aad98De9cc2794593B68ecD4aa5f81076";
+const bridgeName: IBridgeName = "cctp";
+
+const splitRecipients = [
+  "0xFffffffffffffffffffffffffffffffffffffffff",
+  "0xFffffffffffffffffffffffffffffffffffffffff",
+] as Address[];
+const splitPercentages = [95, 5];
 
 // CCTP to transfer from Solana to Base
 const solanaPrivateKeyUint8Array = bs58.decode(solanaPrivateKeyB58);
 const solanaSigner = Keypair.fromSecretKey(solanaPrivateKeyUint8Array);
-const params = {
-    solanaSigner,
-    solanaTokenAddress: usdcMintAddress,
-    amount: usdcAmount,
-    recipientChain,
-    recipientAddress,
-    bridgeName,
-    solanaNetwork,
-    solanaRpcUrl,
+
+console.log(
+  "About to bridge from Solana to " +
+    recipientChain +
+    ": " +
+    solanaSigner.publicKey.toBase58() +
+    " -> " +
+    recipientAddress
+);
+
+// Create a locker splits client.
+const splitsClient = await createLockerSplitClient({
+  apiKey: process.env.LOCKER_API_KEY as string,
+  chain: baseSepolia,
+  signer: LocalAccountSigner.privateKeyToAccountSigner(
+    process.env.PRIV_KEY as `0x${string}`
+  ),
+});
+
+const isSplitsPluginInstalled = await splitsClient.isSplitsPluginInstalled();
+if (!isSplitsPluginInstalled) {
+  const res = await splitsClient.installSplitPlugin(
+    CIRCLE_CONFIG[baseSepolia].usdcAddress,
+    splitPercentages,
+    splitRecipients
+  );
+  console.log("Split plugin installed with:", res);
 }
 
-console.log("About to bridge from Solana to " + recipientChain + ": " + solanaSigner.publicKey.toBase58() + " -> " + recipientAddress)
+const params = {
+  solanaSigner,
+  solanaTokenAddress: usdcMintAddress,
+  amount: usdcAmount,
+  recipientChain,
+  recipientAddress,
+  bridgeName,
+  solanaNetwork,
+  solanaRpcUrl,
+};
+
+// TODO: Use splitsClient to execute bridging
+
 const response: IBridgeFromSolanaResponse = await bridgeTokenFromSolana(params) as ICctpBridgeFromSolanaResponse
 console.log("Bridge tx submitted. Next step is to withdraw the funds on the recipient chain.")
 console.log(response)

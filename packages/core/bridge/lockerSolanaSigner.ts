98918c9f023fb1a687a890d0d39d2bd3b757e32e
thisisashugupta <ashugpt1812@gmail.com>
2025-04-06 22:00:20 +0530

    chore(core): remove unused comments

--- packages/core/bridge/lockerSolanaSigner.ts
+++ packages/core/bridge/lockerSolanaSigner.ts
@@ -1,6 +1,5 @@
 import { Keypair, Transaction } from "@solana/web3.js";
 import { getPublicKey } from "./providers/cctp/cctpUtils";
-// import { PublicKey, type Signer } from "@solana/web3.js";
 import type { VersionedTransaction } from "@solana/web3.js";
 import type { SolanaSigner } from "@account-kit/signer";
 
@@ -22,43 +21,3 @@ export async function createLockerSolanaSigner(signer: Keypair | SolanaSigner) {
     };
     return lockerSolanaSigner;
 }
-
-// class LockerSolanaSigner {
-//     private _signer: Keypair | SolanaSigner;
-//     private _address: string;
-
-//     public addSignature: (data: Transaction | VersionedTransaction) => Promise<Transaction | VersionedTransaction>;
-
-//     get address() {
-//         return this._address;
-//     }
-
-//     get publicKey(): PublicKey {
-//         if (this._signer instanceof Keypair) {
-//             return this._signer.publicKey;
-//         } else {
-//             return new PublicKey(this._signer.address);
-//         }
-//     }
-
-//     constructor(signer: Keypair | SolanaSigner) {
-//         this._signer = signer; // Store the signer instance
-
-//         if (signer instanceof Keypair) {
-//             this._address = signer.publicKey.toBase58();
-//             // Define addSignature for Keypair case
-//             this.addSignature = async (tx: Transaction | VersionedTransaction) => {
-//                 if (tx instanceof Transaction) {
-//                   tx.partialSign(this._signer as unknown as Signer);
-//                 } else {
-//                   tx.sign([this._signer as unknown as Signer]);
-//                 }
-//                 return tx;
-//             };
-//         } else {
-//             this._address = signer.address;
-//             // Bind the existing addSignature from SolanaSigner
-//             this.addSignature = signer.addSignature.bind(signer);
-//         }
-//     }
-// }

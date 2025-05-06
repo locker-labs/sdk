147cf49451257dc4b8c3f78fa8b6ff8a4310b8ab
Mani Brar <manibrar@Manis-MacBook-Pro.local>
2025-05-06 21:35:34 +0530

    fix(core): xLock plugin name

--- packages/core/plugins/defs/xLock/config.ts
+++ packages/core/plugins/defs/xLock/config.ts
@@ -4,7 +4,7 @@ import { MultiOwnerPluginGenConfig } from "../multi-owner/config.js";
 import { XLockAbi } from "./abi.js";
 
 export const XLockPluginGenConfig: PluginConfig = {
-  name: "SplitPlugin",
+  name: "XLockPlugin",
   abi: XLockAbi,
   addresses: {
     [sepolia.id]: "0xAeA689d136d8Fd7098aC8f2c81aEFE1c34c92E3A",
